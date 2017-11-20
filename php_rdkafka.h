caad718821bb589f31ace22c10fd930a64ad0a6f
Arnaud Le Blanc <arnaud.lb@gmail.com>
2017-11-20 11:54:22 +0100

    Back to dev

--- php_rdkafka.h
+++ php_rdkafka.h
@@ -49,7 +49,7 @@ PHP_METHOD(RdKafka, __construct);
 extern zend_module_entry rdkafka_module_entry;
 #define phpext_rdkafka_ptr &rdkafka_module_entry
 
-#define PHP_RDKAFKA_VERSION "3.0.5"
+#define PHP_RDKAFKA_VERSION "3.0.6-dev"
 
 extern zend_object_handlers kafka_default_object_handlers;
 extern zend_class_entry * ce_kafka_exception;
