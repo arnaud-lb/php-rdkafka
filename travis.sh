d728e8ce8f7a488b87199b8307776432364b9d5a
Arnaud Le Blanc <arnaud.lb@gmail.com>
2019-04-18 10:56:02 +0200

    travis: Enable --show-mem only when supported

--- travis.sh
+++ travis.sh
@@ -21,5 +21,11 @@ phpize
 ./configure
 make
 
+showmem=
+if grep -q 'cfgfiles.*mem' run-tests.php; then
+    echo "Will enable the --show-mem flag"
+    showmem=--show-mem
+fi
+
 PHP=$(which php)
-REPORT_EXIT_STATUS=1 TEST_PHP_EXECUTABLE="$PHP" "$PHP" run-tests.php -q -m --show-diff --show-mem
+REPORT_EXIT_STATUS=1 TEST_PHP_EXECUTABLE="$PHP" "$PHP" run-tests.php -q -m --show-diff $showmem
