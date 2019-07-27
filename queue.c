046747083c226158094c804b84944be6e97eea8e
nick <nickjobszh@gmail.com>
2019-07-27 16:44:28 +0200

    replace errno2err calls with last_error

--- queue.c
+++ queue.c
@@ -106,7 +106,7 @@ PHP_METHOD(RdKafka__Queue, consume)
     message = rd_kafka_consume_queue(intern->rkqu, timeout_ms);
 
     if (!message) {
-        err = rd_kafka_errno2err(errno);
+        err = rd_kafka_last_error();
         if (err == RD_KAFKA_RESP_ERR__TIMED_OUT) {
             return;
         }
