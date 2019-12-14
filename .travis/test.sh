1d4746c46709799d3cfb9c42e1818cb0608dfb11
nick <nickjobszh@gmail.com>
2019-12-14 23:41:28 +0100

    add test envs

--- .travis/test.sh
+++ .travis/test.sh
@@ -6,4 +6,9 @@ if grep -q 'cfgfiles.*mem' run-tests.php; then
     echo "Will enable the --show-mem flag"
     showmem=--show-mem
 fi
-docker run -e "TEST_PHP_EXECUTABLE=/usr/local/bin/php" rdkafka-${PHP_IMAGE_TAG}-${LIBRDKAFKA_VERSION} "php" "run-tests.php" "-q" "-m" "--show-diff" "$showmem"
+docker run \
+  -e TEST_KAFKA_BROKERS=${TEST_KAFKA_BROKERS} \
+  -e TEST_KAFKA_BROKER_VERSION=${TEST_KAFKA_BROKER_VERSION} \
+  -e "TEST_PHP_EXECUTABLE=/usr/local/bin/php" \
+  rdkafka-${PHP_IMAGE_TAG}-${LIBRDKAFKA_VERSION} \
+  "php" "run-tests.php" "-q" "-m" "--show-diff" "$showmem"
