f8dc5a718d6e983feefb44446eae3b1d5cf34071
nick <nickjobszh@gmail.com>
2019-12-15 00:38:23 +0100

    minor fixes

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -xve
 
-showmem=
-if grep -q 'cfgfiles.*mem' run-tests.php; then
+if grep -q 'cfgfiles.*mem' ${TRAVIS_BUILD_DIR}/run-tests.php; then
     echo "Will enable the --show-mem flag"
-    showmem=--show-mem
+    exprt SHOWMEM=--show-mem
 fi
+
 docker run \
   --network="host" \
   -e TEST_KAFKA_BROKERS=${TEST_KAFKA_BROKERS} \
   -e TEST_KAFKA_BROKER_VERSION=${TEST_KAFKA_BROKER_VERSION} \
   -e "TEST_PHP_EXECUTABLE=/usr/local/bin/php" \
   rdkafka-${PHP_IMAGE_TAG}-${LIBRDKAFKA_VERSION} \
-  "php" "run-tests.php" "-q" "-m" "--show-diff" "$showmem"
+  "php" "run-tests.php" "-q" "-m" "--show-diff" "${SHOWMEM}"
 
 exit $?
