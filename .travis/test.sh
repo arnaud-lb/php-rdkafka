64cdf382e93f43ed78a52dc3e355ad8140a0b8b6
nick <nickjobszh@gmail.com>
2019-12-15 00:00:29 +0100

    fix exit status

--- .travis/test.sh
+++ .travis/test.sh
@@ -13,3 +13,5 @@ docker run \
   -e "TEST_PHP_EXECUTABLE=/usr/local/bin/php" \
   rdkafka-${PHP_IMAGE_TAG}-${LIBRDKAFKA_VERSION} \
   "php" "run-tests.php" "-q" "-m" "--show-diff" "$showmem"
+
+exit $?
