f04d671ec69de1e608952cc4f23cf1826e4ff1e5
nick <nickjobszh@gmail.com>
2019-12-14 23:15:49 +0100

    fix build

--- .travis/start-kafka.sh
+++ .travis/start-kafka.sh
@@ -3,7 +3,6 @@ mkdir -p ${HOME}/kafka
 if [ ! -f "/kafka/kafka_2.12-2.3.0.tgz" ]; then
     wget -O ${HOME}/kafka/kafka_2.12-2.3.0.tgz http://ftp.man.poznan.pl/apache/kafka/2.3.0/kafka_2.12-2.3.0.tgz;
 fi
-cd ${HOME}/kafka
-tar -xzf kafka_2.12-2.3.0.tgz
+tar -xzf ${HOME}/kafka/kafka_2.12-2.3.0.tgz
 kafka_2.12-2.3.0/bin/zookeeper-server-start.sh -daemon kafka_2.12-2.3.0/config/zookeeper.properties
 kafka_2.12-2.3.0/bin/kafka-server-start.sh -daemon kafka_2.12-2.3.0/config/server.properties
--- .travis/test.sh
+++ .travis/test.sh
@@ -6,5 +6,4 @@ if grep -q 'cfgfiles.*mem' run-tests.php; then
     echo "Will enable the --show-mem flag"
     showmem=--show-mem
 fi
-
-docker run rdkafka-${PHP_IMAGE_TAG}-${LIBRDKAFKA_VERSION} "php" "run-tests.php" "-q" "-m" "--show-diff" "$showmem"
+docker run -e "TEST_PHP_EXECUTABLE=/usr/local/bin/php" rdkafka-${PHP_IMAGE_TAG}-${LIBRDKAFKA_VERSION} "php" "run-tests.php" "-q" "-m" "--show-diff" "$showmem"
