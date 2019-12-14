2bdd6f5e9a5713eb719dbf42803c729b4c5c2678
nick <nickjobszh@gmail.com>
2019-12-14 23:01:16 +0100

    fix caching

--- .travis/start-kafka.sh
+++ .travis/start-kafka.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-mkdir -p /kafka
+mkdir -p ${HOME}/kafka
 if [ ! -f "/kafka/kafka_2.12-2.3.0.tgz" ]; then
-    wget -O /kafka/kafka_2.12-2.3.0.tgz http://ftp.man.poznan.pl/apache/kafka/2.3.0/kafka_2.12-2.3.0.tgz;
+    wget -O ${HOME}/kafka/kafka_2.12-2.3.0.tgz http://ftp.man.poznan.pl/apache/kafka/2.3.0/kafka_2.12-2.3.0.tgz;
 fi
-tar -xzf /kafka/kafka_2.12-2.3.0.tgz
-/kafka/kafka_2.12-2.3.0/bin/zookeeper-server-start.sh -daemon kafka_2.12-2.3.0/config/zookeeper.properties
-/kafka/kafka_2.12-2.3.0/bin/kafka-server-start.sh -daemon kafka_2.12-2.3.0/config/server.properties
+tar -xzf ${HOME}/kafka/kafka_2.12-2.3.0.tgz
+${HOME}/kafka/kafka_2.12-2.3.0/bin/zookeeper-server-start.sh -daemon kafka_2.12-2.3.0/config/zookeeper.properties
+${HOME}/kafka/kafka_2.12-2.3.0/bin/kafka-server-start.sh -daemon kafka_2.12-2.3.0/config/server.properties
