cf2abbb9eeb9ff4568d76139cc5a0b6b6c6709ed
nick <nickjobszh@gmail.com>
2019-12-15 00:40:21 +0100

    minor fix

--- .travis/test.sh
+++ .travis/test.sh
@@ -11,6 +11,7 @@ docker run \
   -e TEST_KAFKA_BROKERS=${TEST_KAFKA_BROKERS} \
   -e TEST_KAFKA_BROKER_VERSION=${TEST_KAFKA_BROKER_VERSION} \
   -e "TEST_PHP_EXECUTABLE=/usr/local/bin/php" \
+  -e "REPORT_EXIT_STATUS=1" \
   rdkafka-${PHP_IMAGE_TAG}-${LIBRDKAFKA_VERSION} \
   "php" "run-tests.php" "-q" "-m" "--show-diff" "${SHOWMEM}"
 
