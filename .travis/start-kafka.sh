52a0559d9e0d7e491b39eeae2f7666e41ab031d6
nick <nickjobszh@gmail.com>
2019-12-14 22:57:48 +0100

    fix build

--- .travis/start-kafka.sh
+++ .travis/start-kafka.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
+mkdir -p /kafka
 if [ ! -f "/kafka/kafka_2.12-2.3.0.tgz" ]; then
-    mkdir /kafka;cd /kafka;wget http://ftp.man.poznan.pl/apache/kafka/2.3.0/kafka_2.12-2.3.0.tgz;
+    wget -O /kafka/kafka_2.12-2.3.0.tgz http://ftp.man.poznan.pl/apache/kafka/2.3.0/kafka_2.12-2.3.0.tgz;
 fi
 tar -xzf /kafka/kafka_2.12-2.3.0.tgz
 /kafka/kafka_2.12-2.3.0/bin/zookeeper-server-start.sh -daemon kafka_2.12-2.3.0/config/zookeeper.properties
