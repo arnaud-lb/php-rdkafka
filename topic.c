2aadeaddf48f2f1f310b3152c657c49177176a7d
nick <nickjobszh@gmail.com>
2020-01-03 09:49:18 +0100

    fix topic deconstruct for high level

--- topic.c
+++ topic.c
@@ -48,8 +48,8 @@ static void kafka_topic_free(zend_object *object TSRMLS_DC) /* {{{ */
     kafka_topic_object *intern = get_custom_object(kafka_topic_object, object);
 
     if (intern->rkt) {
-        kafka_object *kafka_intern = get_kafka_object(P_ZEVAL(intern->zrk) TSRMLS_CC);
-        if (kafka_intern) {
+        kafka_object *kafka_intern = get_custom_object_zval(kafka_object, P_ZEVAL(intern->zrk));
+        if (kafka_intern->rk) {
             zend_hash_index_del(&kafka_intern->topics, (zend_ulong)intern);
         }
     }
