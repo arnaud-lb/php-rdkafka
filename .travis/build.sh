e92049a3cadb035459b1b49e20363cf5e6f12fe4
Paweł Niedzielski <pawel.tadeusz.niedzielski@gmail.com>
2020-02-17 23:44:12 +0100

    Speed up travis builds

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 
-set -xve
+set -e
 
-if ! [ -d "librdkafka" ]; then
-  git clone --depth 1 --branch "${LIBRDKAFKA_VERSION:-v1.2.2}" "${LIBRDKAFKA_REPOSITORY_URL:-https://github.com/edenhill/librdkafka.git}"
-fi
-
-(
-  cd librdkafka
-  ./configure
-  make
-  sudo make install
-)
 sudo ldconfig
 
 echo "extension = $(pwd)/modules/rdkafka.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
