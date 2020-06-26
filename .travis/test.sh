fdfbc2f4605d776294890b7d00b5172a818f5573
Arnaud Le Blanc <arnaud.lb@gmail.com>
2020-06-26 17:09:28 +0200

    .

--- .travis/test.sh
+++ .travis/test.sh
@@ -4,11 +4,11 @@ set -xve
 
 export PATH=$TRAVIS_BUILD_DIR/.travis:$PATH
 
-showmem=
-if grep -q 'cfgfiles.*mem' run-tests.php; then
-    echo "Will enable the --show-mem flag"
-    showmem=--show-mem
+if [ -n "$MEMORY_CHECK" ]; then
+      echo "Enabling memory checking"
+      showmem=--show-mem
+      checkmem=-m
 fi
 
 PHP=$(which php)
-REPORT_EXIT_STATUS=1 TEST_PHP_EXECUTABLE="$PHP" "$PHP" run-tests.php -q -m --show-diff $showmem
+REPORT_EXIT_STATUS=1 TEST_PHP_EXECUTABLE="$PHP" "$PHP" run-tests.php -q $checkmem --show-diff $showmem
