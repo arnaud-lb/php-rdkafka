b3bfa61ef33285b82e785e8f8b0946801ebcce84
Nick <nickjobszh@gmail.com>
2021-07-02 11:40:44 +0200

    replace deprecated interfaces (#460)
    * replace deprecated interfaces
    
    * fix for 7.0

--- metadata_collection.c
+++ metadata_collection.c
@@ -275,7 +275,11 @@ void kafka_metadata_collection_minit(INIT_FUNC_ARGS)
     INIT_NS_CLASS_ENTRY(tmpce, "RdKafka\\Metadata", "Collection", fe);
     ce = zend_register_internal_class(&tmpce);
     ce->create_object = create_object;
+#if PHP_VERSION_ID < 80100
     zend_class_implements(ce, 2, spl_ce_Countable, spl_ce_Iterator);
+#else
+    zend_class_implements(ce, 2, zend_ce_countable, zend_ce_iterator);
+#endif
 
     handlers = kafka_default_object_handlers;
     handlers.get_debug_info = get_debug_info;
