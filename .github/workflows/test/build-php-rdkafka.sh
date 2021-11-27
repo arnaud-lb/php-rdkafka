e8e008e10f32539fb1015b8770365f8542c8cb87
Arnaud Le Blanc <arnaud.lb@gmail.com>
2021-11-27 13:53:41 +0100

    Add package action

--- .github/workflows/test/build-php-rdkafka.sh
+++ .github/workflows/test/build-php-rdkafka.sh
@@ -5,13 +5,16 @@ set -e
 echo "Building php-rdkafka with PHP version:"
 php --version
 
+cd php-rdkafka
+
+PACKAGE_VERSION="$(grep -m 1 '<release>' package.xml|cut -d'>' -f2|cut -d'<' -f1)"
+
+pecl package
+
 if [ $MEMORY_CHECK -eq 1 ]; then
     PHP_RDKAFKA_CFLAGS="-Wall -Werror -Wno-deprecated-declarations"
 fi
 
-cd php-rdkafka
-phpize
-CFLAGS="$PHP_RDKAFKA_CFLAGS" ./configure
-make
+sudo CFLAGS="$PHP_RDKAFKA_CFLAGS" pecl install "./rdkafka-$PACKAGE_VERSION.tgz"
 
-echo "extension=$(pwd)/modules/rdkafka.so"|sudo tee /usr/local/etc/php/rdkafka.ini >/dev/null
+echo "extension=rdkafka.so"|sudo tee /usr/local/etc/php/rdkafka.ini >/dev/null
--- .github/workflows/test/tests.sh
+++ .github/workflows/test/tests.sh
@@ -4,7 +4,7 @@ set -xve
 
 cd php-rdkafka
 
-if [ $MEMORY_CHECK -eq 1 ]; then
+if [ ${MEMORY_CHECK:-0} -eq 1 ]; then
       echo "Enabling memory checking"
       showmem=--show-mem
       checkmem=-m
@@ -13,4 +13,4 @@ fi
 cp tests/test_env.php.sample tests/test_env.php
 
 PHP=$(which php)
-REPORT_EXIT_STATUS=1 TEST_PHP_EXECUTABLE="$PHP" "$PHP" run-tests.php -q $checkmem --show-diff $showmem
+sudo REPORT_EXIT_STATUS=1 TEST_PHP_EXECUTABLE="$PHP" "$PHP" /usr/local/lib/php/build/run-tests.php -q $checkmem --show-diff $showmem
