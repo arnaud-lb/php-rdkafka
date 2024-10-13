b92336be6d7791834009194faa622290409e3977
Sarina Corrigan <26685104+scorgn@users.noreply.github.com>
2024-10-13 19:49:03 -0400

    Skip oauthbearer tests and setup steps if librdkafka version does not support it
    * Add new test env var SKIP_OAUTH based on matrix.skipoauth
    * Set matrix.skipoauth on all librdkafka versions below v1.1.0
    * Don't set up kafka_oauth2 container if SKIP_OAUTH is 1
    * Skip tests in oauthbearer_integration.phpt if RD_KAFKA_VERSION is below 0x010100

--- .github/workflows/test/start-kafka.sh
+++ .github/workflows/test/start-kafka.sh
@@ -13,21 +13,23 @@ docker run -d -p 9092:9092 --network kafka_network \
     -e "KAFKA_ADVERTISED_PORT=9092" \
     --name kafka wurstmeister/kafka:latest
 
-docker run -d -p 29092:29092 --network kafka_network \
-    -e "KAFKA_AUTO_CREATE_TOPICS_ENABLE=true" \
-    -e "KAFKA_CREATE_TOPICS=test-topic:1:1:compact" \
-    -e "KAFKA_ADVERTISED_HOST_NAME=kafka_oauth2" \
-    -e "KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181/kafka_oauth2" \
-    -e "KAFKA_ADVERTISED_PORT=29092" \
-    -e "KAFKA_BROKER_ID=2" \
-    -e "KAFKA_LISTENERS=SASLPLAINTEXT://kafka_oauth2:29092" \
-    -e "KAFKA_ADVERTISED_LISTENERS=SASLPLAINTEXT://kafka_oauth2:29092" \
-    -e "KAFKA_LISTENER_NAME_SASLPLAINTEXT_OAUTHBEARER_SASL_JAAS_CONFIG=org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required unsecuredValidatorRequiredScope=\"required-scope\" unsecuredLoginStringClaim_scope=\"required-scope\" unsecuredLoginStringClaim_sub=\"admin\";" \
-    -e "KAFKA_INTER_BROKER_LISTENER_NAME=SASLPLAINTEXT" \
-    -e "KAFKA_SASL_ENABLED_MECHANISMS=OAUTHBEARER" \
-    -e "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=SASLPLAINTEXT:SASL_PLAINTEXT" \
-    -e "KAFKA_SASL_MECHANISM_INTER_BROKER_PROTOCOL=OAUTHBEARER" \
-    --name kafka_oauth2 wurstmeister/kafka:latest
+if [ ${SKIP_OAUTH:-0} -ne 1 ]; then
+  docker run -d -p 29092:29092 --network kafka_network \
+      -e "KAFKA_AUTO_CREATE_TOPICS_ENABLE=true" \
+      -e "KAFKA_CREATE_TOPICS=test-topic:1:1:compact" \
+      -e "KAFKA_ADVERTISED_HOST_NAME=kafka_oauth2" \
+      -e "KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181/kafka_oauth2" \
+      -e "KAFKA_ADVERTISED_PORT=29092" \
+      -e "KAFKA_BROKER_ID=2" \
+      -e "KAFKA_LISTENERS=SASLPLAINTEXT://kafka_oauth2:29092" \
+      -e "KAFKA_ADVERTISED_LISTENERS=SASLPLAINTEXT://kafka_oauth2:29092" \
+      -e "KAFKA_LISTENER_NAME_SASLPLAINTEXT_OAUTHBEARER_SASL_JAAS_CONFIG=org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required unsecuredValidatorRequiredScope=\"required-scope\" unsecuredLoginStringClaim_scope=\"required-scope\" unsecuredLoginStringClaim_sub=\"admin\";" \
+      -e "KAFKA_INTER_BROKER_LISTENER_NAME=SASLPLAINTEXT" \
+      -e "KAFKA_SASL_ENABLED_MECHANISMS=OAUTHBEARER" \
+      -e "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=SASLPLAINTEXT:SASL_PLAINTEXT" \
+      -e "KAFKA_SASL_MECHANISM_INTER_BROKER_PROTOCOL=OAUTHBEARER" \
+      --name kafka_oauth2 wurstmeister/kafka:latest
+fi
 
 printf "\n127.0.0.1  kafka\n127.0.0.1  kafka_oauth2\n"|sudo tee /etc/hosts >/dev/null
 
@@ -43,7 +45,7 @@ for i in $(seq 1 20); do
             echo "Kafka is ready"
         fi
     fi
-    if [ $kafka_oauth2_ready -eq 0 ]; then
+    if [ $kafka_oauth2_ready -eq 0 ] && [ ${SKIP_OAUTH:-0} -ne 1 ]; then
         if kafkacat -b kafka_oauth2:29092 \
             -X security.protocol=SASL_PLAINTEXT \
             -X sasl.mechanisms=OAUTHBEARER \
@@ -55,7 +57,7 @@ for i in $(seq 1 20); do
         fi
     fi
 
-    if [ $kafka_ready -eq 1 ] && [ $kafka_oauth2_ready -eq 1 ]; then
+    if [ $kafka_ready -eq 1 ] && ( [ $kafka_oauth2_ready -eq 1 ] || [ ${SKIP_OAUTH:-0} -eq 1 ] ); then
       exit 0
     fi
 done
